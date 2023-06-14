3d9daf98acd2fee810695b25059f17f4b70fe5d6
Leonardo <leonardoaugusto1000a@gmail.com>
2023-06-14 17:15:29 -0300

    Resolução do erro de importação do materialize / importei direto do CDN

--- config/config.php
+++ config/config.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Importando css direto do cdn-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Curso de MVC com PHP</title>
+    </head>
+
+    <body>
+        <nav class="blue-grey darken-3"></nav>
+
+      <!--Importando o arquivo js direto do cdn-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+  </html>
