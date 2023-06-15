33ad108997153f2f9ab8da7d82f9781e631352bc
Leonardo <leonardoaugusto1000a@gmail.com>
2023-06-14 22:41:40 -0300

    criando o sistema de rotas (router)

--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,26 @@ namespace core;
 
 class Router
 {
+    // criando 3 atributos privados para gerenciar as urls
+    // Um para armazenar a classe
+    // Um para o metodo que vai acessar a pagina 
+    // Um para armazenar o parametro que vai ser passado 
+
+    private $controler = 'Site';
+    private $method = 'home';
+    private $param = '[]';
+
+
     public function __construct()
     {
         echo "carregando a classe router";
+    }
+
+    // Criando uma função para pegar os dados passados pela url do navegador
+    private function url(){
+        // Passando explode, sempre que encontrar uma barra ele quebra
+        // 'router' é o nome da vareavel que foi definida no htaccess
+        $parse_url = explode("/", filter_input(INPUT_GET, 'router', FILTER_SANITIZE_URL));
+        return $parse_url;
     }
 }
