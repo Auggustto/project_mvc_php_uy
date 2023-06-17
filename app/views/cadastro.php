3d5812b87f6603d617125c579e23ef09ee909958
Leonardo <leonardoaugusto1000a@gmail.com>
2023-06-17 02:27:51 -0300

    criando conexão com banco de dados

--- app/views/cadastro.php
+++ app/views/cadastro.php
@@ -7,21 +7,21 @@
     <div class="cols 12">
         <form action="?router=Site/cadastro/" method="post">
             <div class="input-field col s12 m6">
-                <iput type="text" name="nome" id="nome" required></iput>
+                <!-- <iput type="text" name="nome" id="nome" required> -->
+                <input placeholder="Digite seu nome" id="nome" type="text" class="validate" required>
                 <label for="nome">Digite seu nome</label>
             </div>
 
             <div class="input-field col s12 m6">
-                <iput type="email" name="email" id="email" required></iput>
+                <input placeholder="Digite seu e-mail" id="email" type="email" class="validate" required>
                 <label for="email">Digite seu e-mail</label>
             </div>
-
             <div class="input-field col s12 m6">
-                <iput type="tel" name="tel" id="tel" required></iput>
-                <label for="tel">Digite seu telefone</label>
+                <input placeholder="Digite seu telefone" id="tel" type="tel" class="validate" required>
+                <label for="email">Digite seu telefone</label>
             </div>
 
-            <div class="input-field col s12 m6">
+            <div class="input-field col s12">
                 <input type="submit" value="enviar" class="btn-small">
                 <input type="reset" value="limpar" class="btn-small red">
             </div>
--- config/config.php
+++ config/config.php
@@ -33,5 +33,4 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
   <!-- <script src="./materialize/js/materialize.min.js"></script> -->
 </body>
-
 </html>
