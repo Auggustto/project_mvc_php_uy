b7ee3def2075d3e3d3d6e82026dae6ea29c3e2a5
Leonardo <leonardoaugusto1000a@gmail.com>
2023-06-17 01:24:22 -0300

    criando a tela de cadastro

--- app/views/cadastro.php
+++ app/views/cadastro.php
@@ -2,4 +2,30 @@
     <div class="col s12">
         <h3 class="light">Pagina Cadastro.</h3>
     </div>
+
+    <!-- Criando formulario -->
+    <div class="cols 12">
+        <form action="?router=Site/cadastro/" method="post">
+            <div class="input-field col s12 m6">
+                <iput type="text" name="nome" id="nome" required></iput>
+                <label for="nome">Digite seu nome</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+                <iput type="email" name="email" id="email" required></iput>
+                <label for="email">Digite seu e-mail</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+                <iput type="tel" name="tel" id="tel" required></iput>
+                <label for="tel">Digite seu telefone</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+                <input type="submit" value="enviar" class="btn-small">
+                <input type="reset" value="limpar" class="btn-small red">
+            </div>
+        </form>
+    </div>
+
 </div>
--- config/config.php
+++ config/config.php
@@ -6,6 +6,7 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--Importando css direto do cdn-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <!-- <link rel="stylesheet" href="./materialize/css/materialize.min.css"> -->
 
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -30,6 +31,7 @@
 
   <!--Importando o arquivo js direto do cdn-->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <!-- <script src="./materialize/js/materialize.min.js"></script> -->
 </body>
 
 </html>
